cea9a8e2fa4f6bd3a9ceab745daef5c369bb623a
Jorge Jaular <jordijaulas13@gmail.com>
2017-10-27 11:11:59 +0200

    Se ha creado la opción de elegir entre ticket normal y ticket regalo.

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,15 +17,18 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    // Introduce true si quiere comprar un ticket o false si quiere ticket regalo
+    private boolean tipo;
 
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
-    {
+    public TicketMachine(int cost, boolean maquina)
+    {           
         price = cost;
         balance = 0;
         total = 0;
+        tipo = maquina;
     }
 
     /**
@@ -67,7 +70,7 @@ public class TicketMachine
      */
     public void printTicket()
     {
-        if(balance >= price) {
+        if(balance >= price  & tipo == true) {
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
@@ -75,11 +78,30 @@ public class TicketMachine
             System.out.println("# " + price + " cents.");
             System.out.println("##################");
             System.out.println();
+            
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket Regalo");
+            System.out.println("##################");
 
             // Update the total collected with the price.
             total = total + price;
             // Reduce the balance by the prince.
             balance = balance - price;
+        }
+        else if(balance >= price & tipo == false) {
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
+                        
         }
         else {
             System.out.println("You must insert at least: " +
@@ -110,8 +132,8 @@ public class TicketMachine
         devolverDinero = 0;
         if(balance == 0) {            
             int devolverPasta;
-            devolverPasta = total + balance;
-            balance = 0;            
+            devolverPasta = total;
+            total = 0;            
             devolverDinero = devolverPasta;
         }
         else {
